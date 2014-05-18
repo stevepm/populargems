81af38b16e56548dc483ebfcbb8f5fd05406fabd
stevepm <steven.magelowitz@gmail.com>
2014-05-18 16:24:20 -0600

    implemented signin with github

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140518200241) do
+ActiveRecord::Schema.define(version: 20140518214601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20140518200241) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "project_url"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
