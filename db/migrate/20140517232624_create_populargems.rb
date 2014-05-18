a42d45821e239c025343e19d1bcdeb801abd455f
stevepm <steven.magelowitz@gmail.com>
2014-05-17 20:27:03 -0600

    Created popular_gems table

---
+++ db/migrate/20140517232624_create_populargems.rb
@@ -0,0 +1,14 @@
+class CreateRubygems < ActiveRecord::Migration
+  def change
+    create_table :popular_gems do |t|
+      t.string :name
+      t.string :version
+      t.integer :total_downloads
+      t.integer :version_downloads
+      t.string :description
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
