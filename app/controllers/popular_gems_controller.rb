5bd8c642dbbed4cc09234d692dfc0901e06942e3
stevepm <steven.magelowitz@gmail.com>
2014-05-17 20:27:37 -0600

    Users can see popular gems on homepage and search

--- app/controllers/popular_gems_controller.rb
+++ app/controllers/popular_gems_controller.rb
@@ -1,9 +1,28 @@
 class PopularGemsController < ApplicationController
   def index
     @popular_gem = PopularGem.new
+    @gems = PopularGem.all
   end
 
   def create
+    @popular_gem = PopularGem.create(gem_params)
+    if @popular_gem.valid?
+      redirect_to @popular_gem
+    elsif @popular_gem.errors[:name].first == "has already been taken"
+      @popular_gem = PopularGem.find_by_name(@popular_gem.name)
+      redirect_to @popular_gem
+    else
+      @gems = PopularGem.all
+      render :index, notice: "That gem does not exist"
+    end
+  end
 
+  def show
+    @popular_gem = PopularGem.find(params[:id])
+  end
+
+  private
+  def gem_params
+    params.require(:popular_gem).permit(:name)
   end
 end
