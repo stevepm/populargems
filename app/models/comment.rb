8608049fba12a7877ddd2a5f5da8b28a311b304a
stevepm <steven.magelowitz@gmail.com>
2014-05-19 23:55:36 -0600

    User can submit comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :popular_gem
+
+  def self.create_from_gem_page(params)
+    create! do |comment|
+      comment.body = params[:body]
+      comment.user = User.find(params[:user])
+      comment.popular_gem = PopularGem.find(params[:popular_gem])
+    end
+  end
 end
