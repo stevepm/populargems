df2f2a9eae52c9561ba320a0a21e47d5602b83df
stevepm <steven.magelowitz@gmail.com>
2014-05-21 16:23:46 -0600

    fix to make fonts work on heroku

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ Bundler.require(*Rails.groups)
 module PopularGems
   class Application < Rails::Application
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
+    config.assets.precompile += %w(.svg .eot .woff .ttf)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
