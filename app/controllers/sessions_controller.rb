dd4715c95ad5e97a2ef815b8d24566c7c9c2a4c3
stevepm <steven.magelowitz@gmail.com>
2014-05-22 05:52:03 -0600

    updated specs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
     auth = request.env['omniauth.auth']
     user = User.find_by_provider_and_uid(auth['provider'], auth['uid']) || User.create_with_omniauth(auth)
     log_in(user)
-    redirect_to root_url, :notice => 'Signed in!'
+    redirect_to root_url
   end
 
   def destroy
     log_out
-    redirect_to :back, :notice => 'Signed out!'
+    redirect_to :back
   end
 
 end
