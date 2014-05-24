8d5bd7475265e99fba47b043afb695be90ba1b3b
stevepm <steven.magelowitz@gmail.com>
2014-05-24 01:45:38 -0600

    Allow user to search for a gem that isnt in db

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,10 +1,15 @@
 class SearchesController < ApplicationController
   def show
-    if gem = PopularGem.find_by_name(params[:gem_name])
+    gem_name = params[:gem_name]
+    if gem = PopularGem.find_by_name(gem_name)
       redirect_to gem
     else
-      flash[:notice] = "That gem does not exist"
-      redirect_to :back
+      if gem = GemImporter.seed_db(gem_name)
+        redirect_to gem
+      else
+        flash[:notice] = "That gem does not exist"
+        redirect_to :back
+      end
     end
   end
 end
