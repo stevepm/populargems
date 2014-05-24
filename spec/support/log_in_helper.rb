18eeb37683528af56092e31f9d9e0b25771480b5
stevepm <steven.magelowitz@gmail.com>
2014-05-24 17:13:05 -0600

    Displays user points next to their name

--- spec/support/log_in_helper.rb
+++ spec/support/log_in_helper.rb
@@ -1,7 +1,7 @@
 module LogInHelper
-  def log_in
+  def log_in(username = 'stevepm')
     visit '/'
-    mock_auth_hash
+    mock_auth_hash(username)
     click_link 'Sign in with Github'
   end
 end
