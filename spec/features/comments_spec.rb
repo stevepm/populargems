045dad27f8341ce551f8a9cd99ab08a95dc185aa
stevepm <steven.magelowitz@gmail.com>
2014-05-24 17:13:05 -0600

    Displays user points next to their name

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -7,11 +7,30 @@ describe 'commenting' do
   it 'a user can leave a comment if logged in' do
     log_in
     click_link 'faraday'
+    within '#points' do
+      expect(page).to have_content('0')
+    end
     fill_in 'comment_body', with: "This is *bongos*, indeed."
     click_on 'Comment'
+    within '#points' do
+      expect(page).to have_content('20')
+    end
     expect(page).to have_content('stevepm:')
     expect(page).to have_content('This is bongos, indeed.')
     click_on 'Delete comment'
+    within '#points' do
+      expect(page).to have_content('10')
+    end
     expect(page).to_not have_content('This is bongos, indeed.')
+    click_on 'Sign out'
+    log_in
+    within '#points' do
+      expect(page).to have_content('10')
+    end
+    click_on 'Sign out'
+    log_in('test')
+    within '#points' do
+      expect(page).to have_content('0')
+    end
   end
 end
