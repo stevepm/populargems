092fee8f7eb5377332468351ea78dd6ec5da99a9
stevepm <steven.magelowitz@gmail.com>
2014-05-28 15:48:06 -0600

    Elastic search is functional

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,15 +1,16 @@
 class SearchesController < ApplicationController
   def show
-    gem_name = params[:gem_name]
-    if gem = PopularGem.find_by_name(gem_name)
-      redirect_to gem
-    else
-      if gem = GemImporter.seed_db(gem_name)
-        redirect_to gem
-      else
-        flash[:notice] = "That gem does not exist"
-        redirect_to :back
-      end
-    end
+    response = PopularGem.search params[:gem_name]
+    @results = response.records
+    # if gem = PopularGem.find_by_name(gem_name)
+    #   redirect_to gem
+    # else
+    #   if gem = GemImporter.seed_db(gem_name)
+    #     redirect_to gem
+    #   else
+    #     flash[:notice] = "That gem does not exist"
+    #     redirect_to :back
+    #   end
+    # end
   end
 end
--- app/models/popular_gem.rb
+++ app/models/popular_gem.rb
@@ -1,9 +1,32 @@
 class PopularGem < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
   extend FriendlyId
   friendly_id :name, use: :slugged
   has_many :comments
   acts_as_votable
   validates :name, uniqueness: true
+
+  def self.search(query)
+    __elasticsearch__.search({from: 0, size: 150, query: {
+      bool: {
+        should: [
+          multi_match: {
+            query: query,
+            fields: ['name^3', 'description']
+          }
+        ]
+      },
+      filtered: {
+        filter: {
+          range: {
+            total_downloads: {
+              from: 1000
+            }
+          }
+        }
+      }}})
+  end
 
   def self.top_downloaded(limit = nil)
     order(total_downloads: :desc).limit(limit)
