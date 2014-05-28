52fff740488415e23ac8e495839613a2f9641e86
stevepm <steven.magelowitz@gmail.com>
2014-05-28 17:27:41 -0600

    Updated searches view

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,16 +1,6 @@
 class SearchesController < ApplicationController
   def show
     response = PopularGem.search params[:gem_name]
-    @results = response.records
-    # if gem = PopularGem.find_by_name(gem_name)
-    #   redirect_to gem
-    # else
-    #   if gem = GemImporter.seed_db(gem_name)
-    #     redirect_to gem
-    #   else
-    #     flash[:notice] = "That gem does not exist"
-    #     redirect_to :back
-    #   end
-    # end
+    @results = response.records.pagination(params[:page])
   end
 end
