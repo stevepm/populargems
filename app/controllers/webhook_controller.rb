b5b52e8adaf883d7d6c37732e7be8f5a5620c659
stevepm <steven.magelowitz@gmail.com>
2014-05-28 17:05:23 -0600

    Webhooks with rubygem is configured

---
+++ app/controllers/webhook_controller.rb
@@ -0,0 +1,19 @@
+class WebhookController < ApplicationController
+  protect_from_forgery with: :exception, except: :get_data
+
+  def get_data
+    @name = allowed_params[:name]
+
+    if PopularGem.find_by_name(@name)
+      GemImporter.update_gem(@name)
+    else
+      GemImporter.seed_db(@name)
+    end
+    render text: ''
+  end
+
+  private
+  def allowed_params
+    params.permit!
+  end
+end
