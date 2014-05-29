155ffa4eeb7511b8f3f98274b05797737aa4e0bf
stevepm <steven.magelowitz@gmail.com>
2014-05-28 20:07:12 -0600

    Updated search query script

--- app/models/popular_gem.rb
+++ app/models/popular_gem.rb
@@ -8,24 +8,41 @@ class PopularGem < ActiveRecord::Base
   validates :name, uniqueness: true
 
   def self.search(query)
-    __elasticsearch__.search({from: 0, size: 150, query: {
-      bool: {
-        should: [
-          multi_match: {
-            query: query,
-            fields: ['name^3', 'description']
-          }
-        ]
-      },
-      filtered: {
-        filter: {
-          range: {
-            total_downloads: {
-              from: 1000
+    __elasticsearch__.search(
+      {
+        from: 0,
+        size: 150,
+        query: {
+          bool: {
+            should: [
+              function_score: {
+                query: {
+                  multi_match: {
+                    query: query,
+                    fields: ['name^3', 'description']
+                  }
+                },
+                functions: [
+                  script_score: {
+                    script: "_score * doc['total_downloads'].value / 2**3.1"
+                  }
+                ],
+                score_mode: "sum"
+              }
+            ]
+          },
+          filtered: {
+            filter: {
+              range: {
+                total_downloads: {
+                  from: 1000
+                }
+              }
             }
           }
         }
-      }}})
+      }
+    )
   end
 
   def self.top_downloaded(limit = nil)
