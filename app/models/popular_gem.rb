95c0882fd96a4973a707017d689d9f95e397c3fa
stevepm <steven.magelowitz@gmail.com>
2014-06-01 21:31:20 -0600

    update search query

--- app/models/popular_gem.rb
+++ app/models/popular_gem.rb
@@ -37,7 +37,7 @@ class PopularGem < ActiveRecord::Base
                         multi_match: {
                           query: query,
                           fields: ['name^10', 'description'],
-                          type: "best_fields",
+                          type: "phrase"
                         }
                       },
                       functions: [
