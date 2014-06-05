90befc790e597f8c628f00f6dbbac56bf7ca6ef7
stevepm <steven.magelowitz@gmail.com>
2014-06-05 14:37:25 -0600

    Added pingdom script

---
+++ app/assets/javascripts/pingdom.js
@@ -0,0 +1,11 @@
+var _prum = [
+  ['id', '5390d49eabe53d055c7b23c6'],
+  ['mark', 'firstbyte', (new Date()).getTime()]
+];
+(function () {
+  var s = document.getElementsByTagName('script')[0]
+    , p = document.createElement('script');
+  p.async = 'async';
+  p.src = '//rum-static.pingdom.net/prum.min.js';
+  s.parentNode.insertBefore(p, s);
+})();
