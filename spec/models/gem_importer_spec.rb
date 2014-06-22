2239a8ffa88ffb2507abdb2f840f3585129d89bc
stevepm <steven.magelowitz@gmail.com>
2014-06-21 22:48:00 -0600

    Updated spec to support new github api call

--- spec/models/gem_importer_spec.rb
+++ spec/models/gem_importer_spec.rb
@@ -6,9 +6,9 @@ describe GemImporter do
       GemImporter.seed_db('faraday')
       faraday = PopularGem.find_by_name('faraday')
       expect(faraday.name).to eq('faraday')
-      expect(faraday.total_downloads).to eq(8765225)
+      expect(faraday.total_downloads).to eq(9217901)
       expect(faraday.version).to eq('0.9.0')
-      expect(faraday.version_downloads).to eq(709933)
+      expect(faraday.version_downloads).to eq(973495)
       expect(faraday.description).to eq('HTTP/REST API client library.')
       expect(faraday.url).to eq('http://rubygems.org/gems/faraday')
     end
