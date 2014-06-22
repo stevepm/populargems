82c38516027046f14bdab5d1adf9f026639a43e3
stevepm <steven.magelowitz@gmail.com>
2014-06-21 22:16:57 -0600

    Updated github info regex url matcher to support non matching gem names and repo names

--- app/models/github_info.rb
+++ app/models/github_info.rb
@@ -4,9 +4,9 @@ class GithubInfo
       url = gem.project_url
       info = nil
       if url
-        if url_match = url.match(/^[https?:\/\/]?[www.]?github.com\/(\w+)/)
+        if url_match = url.match(/^(?:https?:\/\/)?(?:www.)?github.com\/(\w+)\/(.+)\/?/)
           username = url_match[1]
-          repo = gem.name
+          repo = url_match[2]
           response = Faraday.get("https://api.github.com/repos/#{username}/#{repo}")
           if response.status >= 300
             false
