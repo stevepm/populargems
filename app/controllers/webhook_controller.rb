a3eef6ec191b81d6b47a4567f3d01ac6524067d0
stevepm <steven.magelowitz@gmail.com>
2014-06-25 23:00:39 -0600

    Moved 10k minimum download limit to hook

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -2,11 +2,13 @@ class WebhookController < ApplicationController
   protect_from_forgery with: :exception, except: :get_data
 
   def get_data
-    @name = allowed_params[:name]
-    if PopularGem.find_by_name(@name)
-      GemImporter.update_gem(@name)
-    else
-      GemImporter.seed_db(@name)
+    if allowed_params[:downloads] > 10000
+      @name = allowed_params[:name]
+      if PopularGem.find_by_name(@name)
+        GemImporter.update_gem(@name)
+      else
+        GemImporter.seed_db(@name)
+      end
     end
     render text: ''
   end
